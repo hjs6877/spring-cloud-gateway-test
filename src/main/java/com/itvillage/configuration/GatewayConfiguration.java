f9cce47695fbd5e1b74625b8d681cce23cc0fa09
Kevin <it.village.host@gmail.com>
2024-08-31 22:23:32 +0900

    .

--- src/main/java/com/itvillage/configuration/GatewayConfiguration.java
+++ src/main/java/com/itvillage/configuration/GatewayConfiguration.java
@@ -1,19 +1,46 @@
 package com.itvillage.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfiguration {
+    @Value("${service.order.url}")
+    private String orderServiceUrl;
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
+                .route(r -> r
+                        .path("/orders")
+                        .and()
+                        .method(HttpMethod.POST)
+                        .uri(orderServiceUrl))
+                .route(r -> r
+                        .path("/orders")
+                        .and()
+                        .method(HttpMethod.GET)
+                        .uri(orderServiceUrl))
+                .route(r -> r
+                        .method(HttpMethod.GET)
+                        .and()
+                        .path("/orders/{order-id}")
+                        .filters(f -> f
+                                .rewritePath("/orders/(?<orderId>\\d+)",
+                                        "/orders/${orderId}"))
+                        .uri(orderServiceUrl))
+                .route(r -> r
+                        .method(HttpMethod.GET)
+                        .and()
+                        .path("/members/{member-Id}/orders")
+                        .filters(f -> f
+                                .rewritePath("/members/(?<memberId>\\d+)/orders",
+                                        "/members/${memberId}/orders"))
+                        .uri(orderServiceUrl))
                 .build();
     }
 }
