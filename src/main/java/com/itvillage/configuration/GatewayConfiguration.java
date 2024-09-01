56fa306cb39eb72884340579cb2aaa06981fb3e3
Kevin <it.village.host@gmail.com>
2024-09-01 23:15:14 +0900

    .

--- src/main/java/com/itvillage/configuration/GatewayConfiguration.java
+++ src/main/java/com/itvillage/configuration/GatewayConfiguration.java
@@ -1,17 +1,11 @@
 package com.itvillage.configuration;
 
-import com.itvillage.orders.OrderHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 @Configuration
 public class GatewayConfiguration {
@@ -32,20 +26,8 @@ public class GatewayConfiguration {
                         .method(HttpMethod.GET)
                         .uri(orderServiceUrl))
                 .route(r -> r
-                        .method(HttpMethod.GET)
-                        .and()
-                        .path("/members/{member-Id}/orders")
-                        .filters(f -> f
-                                .rewritePath("/members/(?<memberId>\\d+)/orders",
-                                        "/members/${memberId}/orders"))
-                        .uri(orderServiceUrl))
+                        .path("/graphql")
+                        .uri("http://localhost:8081/graphql"))
                 .build();
     }
-
-    // example-3-combine-orderDetails: 주문 정보, 배달 정보를 조합해서 클라이언트에게 전송하기 위한 OrderHandler로의 라우팅
-    @Bean
-    public RouterFunction<ServerResponse> routeOrderHandler(OrderHandler orderHandler) {
-        return RouterFunctions.route(GET("/orders/{order-id}"), orderHandler::getOrderDetails);
-    }
-
 }
